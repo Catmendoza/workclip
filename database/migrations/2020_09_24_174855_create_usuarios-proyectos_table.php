219aa5ead7d6ea4e5c68a88454dd145c6f69633c
Giovanni Jaramillo <giovanni.jaramillo00@usc.edu.co>
2020-09-24 12:53:12 -0500

    Se creo la tabla usuarios-proyectos y su modelo

---
+++ database/migrations/2020_09_24_174855_create_usuarios-proyectos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosProyectosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+
+    { Schema::create('usuarios-proyectos', function (Blueprint $table) {
+        $table->id('id_proyecto');
+        $table->foreignld('id_perfil');
+        $table->string('imagen');
+        $table->string('nombre_proyecto');
+        $table->text('descripcion');
+        $table->string('estado_proyecto');
+        $table->integer('finalidad_proyecto');
+        $table->date('fecha_publi');
+
+
+        $table->timestamp();
+    });
+    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios-proyectos');
+    }
+}
