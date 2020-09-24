2cbc6cd529e97aba428511c2ad16b4e7aadac8f5
Giovanni Jaramillo <giovanni.jaramillo00@usc.edu.co>
2020-09-24 12:58:48 -0500

    se creo la tabla comentarios y su respectivo modelo

---
+++ database/migrations/2020_09_24_175430_create_comentarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->id('id_comentarios');
+            $table->foreignld('id_producto');
+            $table->foreignld('id_proyecto');
+            $table->foreignld('id_usuario');
+            $table->text('contenido');
+            $table->date('fecha_publi');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+}
