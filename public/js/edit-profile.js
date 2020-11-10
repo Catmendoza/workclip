d3b68fd00b3f62e4ad3303bef2e67605e607028c
juanvillotausc <juan.villota02@usc.edu.co>
2020-11-09 23:28:20 -0500

    Upload Image Implementacion Melo Caramelo

--- public/js/edit-profile.js
+++ public/js/edit-profile.js
@@ -1,14 +1,61 @@
-function readURL(input) {
+var $uploadCrop;
+
+$("#browseImg").on("click", function(ev) {
+    $("#imgpreviewcrop").css("display", "block");
+});
+
+function readFile(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
+
         reader.onload = function(e) {
-            $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-            $('#imagePreview').hide();
-            $('#imagePreview').fadeIn(650);
-        }
+            $(".modalhh").addClass("ready");
+            $uploadCrop
+                .croppie("bind", {
+                    url: e.target.result
+                })
+                .then(function() {
+                    console.log("jQuery bind complete");
+                });
+        };
+
         reader.readAsDataURL(input.files[0]);
+    } else {
+        swal("Sorry - you're browser doesn't support the FileReader API");
     }
 }
-$("#imageUpload").change(function() {
-    readURL(this);
+
+$uploadCrop = $("#imgpreviewcrop").croppie({
+    viewport: {
+        width: 100,
+        height: 100,
+        type: "circle"
+    },
+    enableExif: true
+});
+
+$("#imageUpload").on("change", function() {
+    readFile(this);
+});
+$(".vanilla-result").on("click", function(ev) {
+    $uploadCrop
+        .croppie("result", {
+            type: "canvas",
+            size: "viewport"
+        })
+        .then(function(resp) {
+            $(".avatar-preview").css("background-image", "url(" + resp + ")");
+            $("#imgpreviewcrop").css("display", "none");
+        });
+});
+
+/* Eliminar foto */
+
+$("#DeleteImgBtn").on("click", function(ev) {
+    $("#UserImgBox").css(
+        "background-image",
+        'url("/imagenes_usuarios/usuario.png")'
+    );
+    var UploadDeleteImg = document.getElementById("imageUpload");
+    UploadDeleteImg.value = "/imagenes_usuarios/usuario.png";
 });
--- resources/views/editperfil.blade.php
+++ resources/views/editperfil.blade.php
@@ -1,33 +1,69 @@
 @extends('sidebar')
 
 <link rel="stylesheet" href="{{ asset('css/edit-profile.css') }}" />
-
+<link href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.css"
+    integrity="sha512-zxBiDORGDEAYDdKLuYU9X/JaJo/DPzE42UubfBw9yg8Qvb2YRRIQ8v4KsGHOx2H1/+sdSXyXxLXv5r7tHc9ygg=="
+    crossorigin="anonymous" />
 @section('content-sidebar')
 
-    <form class="" action="{{ route('perfil.update', $datos->id) }}" method="POST"
-        enctype="multipart/form-data">
+    <form class="" action="{{ route('perfil.update', $datos->id) }}" method="POST" enctype="multipart/form-data">
         @method('PATCH')
         @csrf
 
 
-
-
         <div class="container">
-
-            
             <div class="avatar-upload">
                 <div class="avatar-edit">
                     <input type='file' id="imageUpload" name="imagen" accept=".png, .jpg, .jpeg" />
-                    <label for="imageUpload"><i class="fas fa-pencil-alt"></i></label>
+                    <label type="button" id="browseImg" class="btn btn-primary" for="imageUpload" data-toggle="modal"
+                        data-target="#exampleModal"><i class="fas fa-pencil-alt"></i></label>
                 </div>
-                <div class="avatar-preview">
-                    <div id="imagePreview" style="background-image: url({{ asset($datos->imagen) }});">
-                    </div>
+                <div class="avatar-preview" id="UserImgBox" style="background-image: url({{ asset($datos->imagen) }})">
+
+
+
                 </div>
+                <button type="button" id="DeleteImgBtn" class="btn btn-danger">Eliminar</button>
             </div>
+
         </div>
 
+        </div>
 
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="avatar-preview">
+                            <div id="imgpreviewcrop">
+
+                            </div>
+
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="vanilla-result">Save changes</button>
+            </div>
+        </div>
+        </div>
+        </div>
 
 
 
@@ -45,6 +81,32 @@
 
     </form>
 
+    <!-- Button trigger modal -->
+
+
+
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.js"
+        integrity="sha512-vUJTqeDCu0MKkOhuI83/MEX5HSNPW+Lw46BA775bAWIp1Zwgz3qggia/t2EnSGB9GoS2Ln6npDmbJTdNhHy1Yw=="
+        crossorigin="anonymous"></script>
+
+    <script>
+        var imgUser = "<?php echo $datos->imagen; ?>";
+
+    </script>
+
     <script type="text/javascript" src="{{ asset('js/edit-profile.js') }}"></script>
+
+
+
+
+
 @endsection
